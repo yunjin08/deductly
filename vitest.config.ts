ba96d78549c5b1979c618b3db2590cff297dbacc
Maxell Milay <milaymaxell@gmail.com>
2025-01-08 23:33:46 +0800

    [Setup][Maxell] Added pr coverage report workflow

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        coverage: {
+            provider: 'v8',
+            extension: ['.tsx'],
+            reporter: ['text', 'json-summary', 'json'],
+        },
+    },
+});
