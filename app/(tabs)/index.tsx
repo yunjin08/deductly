335ed0d631f62496d8d7f60d587a6528a4853f5b
Maxell Milay <milaymaxell@gmail.com>
2025-01-10 17:02:31 +0800

    [Adjustment][Maxell] Adjusted token visibility in test screen

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,8 +8,7 @@ import { handleSignInWithGoogle } from '@/services/sso/google';
 WebBrowser.maybeCompleteAuthSession();
 
 export default function LoginScreen() {
-    const [userInfo, setUserInfo] = useState(null);
-
+    const [jwtToken, setJwtToken] = useState('No Token');
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [request, response, promptAsync] = Google.useAuthRequest({
         iosClientId: process.env.EXPO_PUBLIC_IOS_CLIENT_ID,
@@ -17,22 +16,24 @@ export default function LoginScreen() {
 
     useEffect(() => {
         if (response) {
-            console.log('INSIDE USE EFFECT');
-            handleSignInWithGoogle(response).then((user) => {
-                setUserInfo(user);
+            handleSignInWithGoogle(response).then((jwtToken) => {
+                setJwtToken(jwtToken);
             });
         }
     }, [response]);
 
+    const resetJWT = async () => {
+        await AsyncStorage.removeItem('@jwt');
+        setJwtToken('No Token');
+    };
+
     return (
         <View style={styles.container}>
-            <Text>{JSON.stringify(userInfo, null, 2)}</Text>
-            <Text>Deductly</Text>
+            <Text>DEDUCTLY</Text>
+            <Text>JWT Token</Text>
+            <Text style={styles.tokenText}>{jwtToken}</Text>
             <Button title="Sign in with Google" onPress={() => promptAsync()} />
-            <Button
-                title="Delete Local Storage"
-                onPress={() => AsyncStorage.removeItem('@user')}
-            />
+            <Button title="Delete Local Storage" onPress={() => resetJWT()} />
         </View>
     );
 }
@@ -44,5 +45,14 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         textAlign: 'center',
+    },
+
+    tokenText: {
+        fontSize: 10,
+        textAlign: 'center',
+        paddingTop: 20,
+        paddingBottom: 20,
+        paddingLeft: 20,
+        paddingRight: 20,
     },
 });
--- services/sso/google.ts
+++ services/sso/google.ts
@@ -5,40 +5,23 @@ import { OAUTH_SUCCESS } from '@/constants/Auth';
 import { AuthSessionResult } from 'expo-auth-session';
 
 export async function handleSignInWithGoogle(response: AuthSessionResult) {
-    const user = await AsyncStorage.getItem('@user');
+    const user = await AsyncStorage.getItem('@jwt');
     if (!user) {
         if (response.type === OAUTH_SUCCESS) {
             const tokenResponse = response.authentication;
             if (tokenResponse) {
-                await axios.post('/sso/google/', {
+                const jwtResponse = await axios.post('/sso/google/', {
                     id_token: tokenResponse.idToken,
                 });
-                console.log('AFTER /sso/google');
 
-                const userInfo = await getUserInfo(tokenResponse.accessToken);
+                const jwtToken = jwtResponse.data.token;
 
-                return userInfo;
+                await AsyncStorage.setItem('@jwt', JSON.stringify(jwtToken));
+
+                return jwtToken;
             }
         }
     } else {
         return user;
     }
 }
-
-export const getUserInfo = async (token: any) => {
-    if (!token) return;
-    try {
-        const response = await axios.get(OAUTH_BASE_URL, {
-            baseURL: '',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-
-        const user = response.data;
-        await AsyncStorage.setItem('@user', JSON.stringify(user));
-        return user;
-    } catch (error) {
-        console.error(error);
-    }
-};
