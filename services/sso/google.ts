ccfcae8f2646bd43e3abac687ba078a5db3c3a7c
Maxell Milay <milaymaxell@gmail.com>
2025-01-10 18:48:30 +0800

    [Feature][Maxell] Integrated persistence through jwt

--- services/sso/google.ts
+++ services/sso/google.ts
@@ -2,11 +2,11 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { OAUTH_BASE_URL } from '@/constants/BaseUrl';
 import { OAUTH_SUCCESS } from '@/constants/Auth';
-import { AuthSessionResult } from 'expo-auth-session';
+import { AccessTokenRequest, AuthSessionResult } from 'expo-auth-session';
 
 export async function handleSignInWithGoogle(response: AuthSessionResult) {
-    const user = await AsyncStorage.getItem('@jwt');
-    if (!user) {
+    const jwt_token = await AsyncStorage.getItem('@jwt');
+    if (!jwt_token) {
         if (response.type === OAUTH_SUCCESS) {
             const tokenResponse = response.authentication;
             if (tokenResponse) {
@@ -18,10 +18,31 @@ export async function handleSignInWithGoogle(response: AuthSessionResult) {
 
                 await AsyncStorage.setItem('@jwt', JSON.stringify(jwtToken));
 
+                const accessToken = tokenResponse.accessToken;
+
+                const email = await getUserInfo(accessToken);
+
+                await AsyncStorage.setItem('@email', email);
+
                 return jwtToken;
             }
         }
     } else {
-        return user;
+        return jwt_token;
     }
 }
+
+export const getUserInfo = async (token: any) => {
+    if (!token) return;
+    try {
+        const response = await axios.get(OAUTH_BASE_URL, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        const user = response.data;
+        return user.email;
+    } catch (error) {
+        console.error(error);
+    }
+};
