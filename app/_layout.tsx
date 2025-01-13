0227028f66d4b450636718d42d3ed32ea5dca5db
Maxell Milay <milaymaxell@gmail.com>
2025-01-13 23:05:38 +0800

    [Adjustment][Maxell] Added closing keyboard when clicking outside it

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,58 @@
 import { Stack } from 'expo-router';
 import axios from 'axios';
-import { LOCALHOST } from '@/constants/BaseUrl';
+import { LOCAL_AREA_HOST } from '@/constants/BaseUrl';
+import {
+    Keyboard,
+    KeyboardAvoidingView,
+    Platform,
+    TouchableWithoutFeedback,
+    StyleSheet,
+} from 'react-native';
 
-axios.defaults.baseURL = LOCALHOST;
+axios.defaults.baseURL = LOCAL_AREA_HOST;
 
 const RootLayout = () => {
+    const dismissKeyboard = () => {
+        Keyboard.dismiss();
+    };
+
     return (
-        <Stack>
-            <Stack.Screen
-                name="index"
-                options={{ headerTitle: 'Welcome', headerShown: false }}
-            />
-            <Stack.Screen
-                name="sign-in"
-                options={{ headerTitle: 'Sign In', headerShown: false }}
-            />
-            <Stack.Screen name="sign-up" options={{ headerTitle: 'Sign Up' }} />
-            <Stack.Screen
-                name="reset-password"
-                options={{ headerTitle: 'Reset Password' }}
-            />
-            <Stack.Screen name="(protected)" options={{ headerShown: false }} />
-        </Stack>
+        <TouchableWithoutFeedback onPress={dismissKeyboard}>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={styles.container}
+            >
+                <Stack>
+                    <Stack.Screen
+                        name="index"
+                        options={{ headerTitle: 'Welcome', headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="sign-in"
+                        options={{ headerTitle: 'Sign In', headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="sign-up"
+                        options={{ headerTitle: 'Sign Up' }}
+                    />
+                    <Stack.Screen
+                        name="reset-password"
+                        options={{ headerTitle: 'Reset Password' }}
+                    />
+                    <Stack.Screen
+                        name="(protected)"
+                        options={{ headerShown: false }}
+                    />
+                </Stack>
+            </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
     );
 };
 
 export default RootLayout;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
