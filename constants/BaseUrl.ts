78005d540987423902ace1555835657c4ae221a5
Maxell Milay <milaymaxell@gmail.com>
2025-01-15 00:57:45 +0800

    [Adjustment][Maxell] Removed constant local area base url

--- constants/BaseUrl.ts
+++ constants/BaseUrl.ts
@@ -1,3 +1,2 @@
 export const OAUTH_BASE_URL = 'https://www.googleapis.com/userinfo/v2/me';
 export const LOCALHOST = 'http://127.0.0.1:8000/api/v1';
-export const LOCAL_AREA_HOST = 'http://192.168.254.208:8000/api/v1';
