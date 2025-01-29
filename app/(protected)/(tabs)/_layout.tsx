654b919daf7f63fdebff448681e7de4e57fc9b0f
yunjin08 <jed.donaire08@gmail.com>
2025-01-29 20:22:03 +0800

    remove layout headers

--- app/(protected)/(tabs)/_layout.tsx
+++ app/(protected)/(tabs)/_layout.tsx
@@ -5,108 +5,111 @@ import { View } from 'react-native';
 
 const TabsLayout = () => {
     return (
-        <Tabs
-            screenOptions={{
-                tabBarStyle: {
-                    height: 84,
-                    paddingTop: 10,
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                },
-                headerShown: true,
-                header: () => <Header />,
-                tabBarActiveTintColor: 'bg-primary',
-                tabBarInactiveTintColor: '#FFFFFF',
-                tabBarBackground: () => <View className="bg-primary h-full" />,
-                tabBarIconStyle: {
-                    marginTop: 0,
-                },
-            }}
-        >
-            <Tabs.Screen
-                name="gallery"
-                options={{
-                    title: 'Gallery',
+        <>
+            <Tabs
+                screenOptions={{
+                    tabBarStyle: {
+                        height: 84,
+                        paddingTop: 10,
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    },
                     headerShown: false,
-                    tabBarLabel: 'Gallery',
-                    tabBarIcon: ({ color, focused }) => (
-                        <FontAwesome
-                            name={focused ? 'image' : 'image'}
-                            size={24}
-                            color={color}
-                        />
+                    tabBarActiveTintColor: '#4CD4E2',
+                    tabBarInactiveTintColor: '#6C757D',
+                    tabBarBackground: () => (
+                        <View className="bg-gray-100 border-t-[1px] border-gray-200 h-full" />
                     ),
-                    header: () => <Header />,
+                    tabBarIconStyle: {
+                        marginTop: 0,
+                    },
                 }}
-            />
-            <Tabs.Screen
-                name="receipts"
-                options={{
-                    title: 'Receipts',
-                    headerShown: false,
-                    tabBarLabel: 'Receipts',
-                    tabBarIcon: ({ color, focused }) => (
-                        <FontAwesome6
-                            name={focused ? 'envelope' : 'envelope'}
-                            size={24}
-                            color={color}
-                        />
-                    ),
-                    header: () => <Header />,
-                }}
-            />
-            <Tabs.Screen
-                name="camera"
-                options={{
-                    title: 'Camera',
-                    headerShown: false,
-                    tabBarLabel: '',
-                    tabBarIcon: () => (
-                        <View className="size-28 border-2 border-white bg-primary rounded-full mb-16 items-center justify-center">
+            >
+                <Tabs.Screen
+                    name="gallery"
+                    options={{
+                        title: 'Gallery',
+                        headerShown: false,
+                        tabBarLabel: 'Gallery',
+                        tabBarIcon: ({ color, focused }) => (
                             <FontAwesome
-                                name="camera"
-                                size={28}
-                                color="white"
+                                name={focused ? 'image' : 'image'}
+                                size={24}
+                                color={color}
                             />
-                        </View>
-                    ),
-                }}
-            />
-            <Tabs.Screen
-                name="documents"
-                options={{
-                    title: 'Documents',
-                    headerShown: false,
-                    tabBarLabel: 'Documents',
-                    tabBarIcon: ({ color, focused }) => (
-                        <FontAwesome6
-                            name={focused ? 'folder' : 'folder'}
-                            size={24}
-                            color={color}
-                        />
-                    ),
-                    header: () => <Header />,
-                }}
-            />
-            <Tabs.Screen
-                name="reports"
-                options={{
-                    title: 'Reports',
-                    headerShown: false,
-                    tabBarLabel: 'Reports',
-                    tabBarIcon: ({ color, focused }) => (
-                        <FontAwesome6
-                            name={focused ? 'chart-line' : 'chart-line'}
-                            size={24}
-                            color={color}
-                        />
-                    ),
-                    header: () => <Header />,
-                }}
-            />
-            <Tabs.Screen name="home" options={{ href: null }} />
-        </Tabs>
+                        ),
+                        header: () => <Header />,
+                    }}
+                />
+                <Tabs.Screen
+                    name="receipts"
+                    options={{
+                        title: 'Receipts',
+                        headerShown: false,
+                        tabBarLabel: 'Receipts',
+                        tabBarIcon: ({ color, focused }) => (
+                            <FontAwesome6
+                                name={focused ? 'envelope' : 'envelope'}
+                                size={24}
+                                color={color}
+                            />
+                        ),
+                        header: () => <Header />,
+                    }}
+                />
+                <Tabs.Screen
+                    name="camera"
+                    options={{
+                        title: 'Camera',
+                        headerShown: false,
+                        tabBarLabel: '',
+                        tabBarIcon: () => (
+                            <View className="size-28 border-2 border-gray-200 bg-primary rounded-full mb-16 items-center justify-center">
+                                <FontAwesome
+                                    name="camera"
+                                    size={28}
+                                    color="white"
+                                />
+                            </View>
+                        ),
+                    }}
+                />
+                <Tabs.Screen
+                    name="documents"
+                    options={{
+                        title: 'Documents',
+                        headerShown: false,
+                        tabBarLabel: 'Documents',
+                        tabBarIcon: ({ color, focused }) => (
+                            <FontAwesome6
+                                name={focused ? 'folder' : 'folder'}
+                                size={24}
+                                color={color}
+                            />
+                        ),
+                        header: () => <Header />,
+                    }}
+                />
+                <Tabs.Screen
+                    name="reports"
+                    options={{
+                        title: 'Reports',
+                        headerShown: false,
+                        tabBarLabel: 'Reports',
+                        tabBarIcon: ({ color, focused }) => (
+                            <FontAwesome6
+                                name={focused ? 'chart-line' : 'chart-line'}
+                                size={24}
+                                color={color}
+                            />
+                        ),
+                        header: () => <Header />,
+                    }}
+                />
+                <Tabs.Screen name="home" options={{ href: null }} />
+            </Tabs>
+        </>
     );
 };
 
