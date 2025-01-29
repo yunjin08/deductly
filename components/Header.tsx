aadcf0c4ee873b3d18194f7ba481028d61c54a7f
yunjin08 <jed.donaire08@gmail.com>
2025-01-29 20:23:11 +0800

    change header

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,16 +3,39 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { FontAwesome6 } from '@expo/vector-icons';
 import { Link } from 'expo-router';
+import { usePathname } from 'expo-router';
 
 const Header = () => {
+    const pathname = usePathname();
+    const showHomeButton = pathname !== '/home';
+
     return (
         <SafeAreaView edges={['top']}>
-            <View style={styles.container}>
-                <Link href="/(protected)/(tabs)/home" asChild>
-                    <TouchableOpacity style={styles.buttonContainer}>
-                        <Text style={styles.text}>Home</Text>
-                    </TouchableOpacity>
-                </Link>
+            <View
+                className={`flex-row py-4 px-6 items-center ${
+                    showHomeButton ? 'justify-between' : 'justify-end'
+                }`}
+            >
+                {showHomeButton && (
+                    <Link href="/(protected)/(tabs)/home" asChild>
+                        <TouchableOpacity>
+                            <View className="flex-row items-center">
+                                <FontAwesome6
+                                    name="chevron-left"
+                                    size={20}
+                                    color="#1fddee"
+                                    className="mr-2"
+                                    solid
+                                />
+                                <View className="bg-[#1fddee] px-4 py-2 rounded-full">
+                                    <Text className="text-white font-semibold text-base">
+                                        Home
+                                    </Text>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    </Link>
+                )}
                 <Link href="/(protected)/profile" asChild>
                     <FontAwesome6
                         name="circle-user"
@@ -29,15 +52,6 @@ const Header = () => {
 export default Header;
 
 const styles = StyleSheet.create({
-    container: {
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingHorizontal: 24,
-        paddingTop: 16, // Add space only at the top
-        paddingBottom: 16, // Minimal bottom padding
-    },
     buttonContainer: {
         alignSelf: 'flex-start',
         backgroundColor: '#1fddee',
