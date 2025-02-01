ba7bd7f8cacb89199bc012cb3336a670fea71cb2
yunjin08 <jed.donaire08@gmail.com>
2025-02-01 12:57:28 +0800

    remove top property header

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,58 +10,41 @@ const Header = () => {
     const showHomeButton = pathname !== '/home';
 
     return (
-        <SafeAreaView edges={['top']}>
-            <View
-                className={`flex-row py-4 px-6 items-center ${
-                    showHomeButton ? 'justify-between' : 'justify-end'
-                }`}
-            >
-                {showHomeButton && (
-                    <Link href="/(protected)/(tabs)/home" asChild>
-                        <TouchableOpacity>
-                            <View className="flex-row items-center">
-                                <FontAwesome6
-                                    name="chevron-left"
-                                    size={20}
-                                    color="#1fddee"
-                                    className="mr-2"
-                                    solid
-                                />
-                                <View className="bg-[#1fddee] px-4 py-2 rounded-full">
-                                    <Text className="text-white font-semibold text-base">
-                                        Home
-                                    </Text>
-                                </View>
+        <View
+            className={`flex-row py-4 px-6 items-center ${
+                showHomeButton ? 'justify-between' : 'justify-end'
+            }`}
+        >
+            {showHomeButton && (
+                <Link href="/(protected)/(tabs)/home" asChild>
+                    <TouchableOpacity>
+                        <View className="flex-row items-center">
+                            <FontAwesome6
+                                name="chevron-left"
+                                size={20}
+                                color="#1fddee"
+                                className="mr-2"
+                                solid
+                            />
+                            <View className="bg-[#1fddee] px-4 py-2 rounded-full">
+                                <Text className="text-white font-semibold text-base">
+                                    Home
+                                </Text>
                             </View>
-                        </TouchableOpacity>
-                    </Link>
-                )}
-                <Link href="/(protected)/profile" asChild>
-                    <FontAwesome6
-                        name="circle-user"
-                        size={32}
-                        color="#1fddee"
-                        solid
-                    />
+                        </View>
+                    </TouchableOpacity>
                 </Link>
-            </View>
-        </SafeAreaView>
+            )}
+            <Link href="/(protected)/profile" asChild>
+                <FontAwesome6
+                    name="circle-user"
+                    size={32}
+                    color="#1fddee"
+                    solid
+                />
+            </Link>
+        </View>
     );
 };
 
 export default Header;
-
-const styles = StyleSheet.create({
-    buttonContainer: {
-        alignSelf: 'flex-start',
-        backgroundColor: '#1fddee',
-        paddingVertical: 8,
-        paddingHorizontal: 16,
-        borderRadius: 20,
-    },
-    text: {
-        color: 'white',
-        fontWeight: '600',
-        fontSize: 15,
-    },
-});
