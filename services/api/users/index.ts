93c76ab846cdcd6b299165326b9f1955477d0a18
yunjin08 <jed.donaire08@gmail.com>
2025-02-08 04:20:03 +0800

    return right data

--- services/api/users/index.ts
+++ services/api/users/index.ts
@@ -4,16 +4,15 @@ import { ChatMessage } from '@/interfaces';
 // Define the chat message type
 
 // Create the chat service with custom methods
-const chatEndpoint = '/chatbot';
+const chatEndpoint = '/chat';
 
 export const chatService = {
     ...createApiService<ChatMessage>(chatEndpoint),
 
     // Send a message and get bot response
     sendMessage: async ({ data }: { data: ChatMessage }) => {
-        const response = await api.post<ChatMessage>(`${chatEndpoint}/send`, {
-            text: data.text,
-            sender: data.sender,
+        const response = await api.post<ChatMessage>(`${chatEndpoint}/`, {
+            question: data.question,
         });
         return response.data;
     },
@@ -21,7 +20,7 @@ export const chatService = {
     // Get chat history, TODO: add pagination and improve security
     getChatHistory: async (id: string) => {
         const response = await api.get<ChatMessage[]>(
-            `${chatEndpoint}/history/${id}`
+            `${chatEndpoint}/history/${id}/`
         );
         return response.data;
     },
