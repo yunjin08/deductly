5094be4283cf32d0a5615b80e6cefe67a0390e8d
yunjin08 <jed.donaire08@gmail.com>
2025-05-21 14:02:12 +0800

    add missing index

--- contexts/reducers/index.ts
+++ contexts/reducers/index.ts
@@ -3,12 +3,14 @@ import authReducer from './authReducers';
 import receiptsReducer from './receiptsReducers';
 import documentsReducer from './documentsReducers';
 import reportsReducer from './reportReducers';
+import galleryReducer from './galleryReducers';
 
 const rootReducer = combineReducers({
     auth: authReducer,
     receipts: receiptsReducer,
     documents: documentsReducer,
     reports: reportsReducer,
+    gallery: galleryReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
